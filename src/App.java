bcb3a47b6499aabe482c9aeb6877a6884b8b694f
rutaeb <iruthroo@gmail.com>
2022-04-19 11:19:02 +0300

    StartFrameDoneNoBGnoStartButton

--- src/App.java
+++ src/App.java
@@ -1,65 +1,56 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.*;  
+import java.awt.LayoutManager;
+import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.plaf.ColorChooserUI;   
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        JFrame frame = new JFrame("Game3");  
+        JFrame frame = new JFrame("Game3");
 
-        JLabel label = new JLabel("Santa Rescue");  
-        label.setBounds(220,150,400,80);
-        label.setFont( new Font("Arial",Font.PLAIN,50));
+        JLabel label = new JLabel("Santa Rescue");
+        label.setBounds(220, 150, 400, 80);
+        label.setFont(new Font("Arial", Font.PLAIN, 50));
         label.setForeground(Color.white);
-        frame.add(label);  
+        frame.add(label);
 
-        JButton buttonStart = new JButton("Start");  
-        buttonStart.setBounds(253,225,253,80);  
-        frame.add(buttonStart); 
+        JButton buttonStart = new JButton("Start");
+        buttonStart.setBounds(253, 225, 253, 80);
+        frame.add(buttonStart);
 
-        JButton buttonExit = new JButton("Exit");  
-        buttonExit.setBounds(253,325,253,80);  
+        JButton buttonExit = new JButton("Exit");
+        buttonExit.setBounds(253, 325, 253, 80);
         frame.add(buttonExit);
-       
-       
-        frame.setSize(800, 800);  
-        frame.setLayout(null);  
 
-        JTextField R, G, B, A;
-        R = new JTextField(3);
-        G = new JTextField(3);
-        B = new JTextField(3);
-        A = new JTextField(3);
-        int r, g, b, a;
- // get rgba value
- r = 128;
- g = 0;
- b = 32;
- a = 255;
+        frame.setSize(800, 800);
+        frame.setLayout(null);
 
- frame.add(R);
- frame.add(G);
- frame.add(B);
- frame.add(A);
- frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        ///////////////////////////////////////
+        // create a new Color
+        Color c = new Color(128, 0, 32, 255);
 
- // create a new Color
- Color c = new Color(r, g, b, a);
+        // set the color as background of panel
+        frame.getContentPane().setBackground(c);
 
- // set the color as background of panel
- frame.getContentPane().setBackground(c);
+        // (gribu izmantot bg1.kpg kā fonu, bet nezinu kā...)
 
-        // frame.getContentPane().setBackground(Color.);
-        frame.setVisible(true);  
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        buttonExit.addActionListener(new ActionListener(){  
-        public void actionPerformed(ActionEvent e){  
-            frame. dispose();          }  
-            }); 
+        buttonExit.addActionListener(new ActionListener() { // Exit Buttona action
+            public void actionPerformed(ActionEvent a) {
+                frame.dispose();
+            }
+        });
 
+        buttonStart.addActionListener(new ActionListener() { // Start Buttona action
+            public void actionPerformed(ActionEvent b) {
+               //aiziet uz jaunu vietu, kur jauni button etc.
+               // bilde ar varoni, zemak buttons un labels etc.
+            }
+        });
 
     }
-
 
 }
